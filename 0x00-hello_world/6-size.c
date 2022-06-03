fb3f87978275ea9cc4dd4a2c5f74f623798db66d
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-03 20:31:30 +0000

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+/* main is Entry poitn */
 int main(void)
 {
 	sizeof(char);
@@ -7,5 +8,13 @@ int main(void)
 	sizeof(long int);
 	sizeof(long long int);
 	sizeof(float);
+
+	{
+		printf("Size of a char: %d byte(s)\n", sizeof(char));
+		printf("Size of an int: %d byte(s)\n", sizeof(int));
+		printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+		printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
+		printf("Size of a float: %d byte(s)\n", sizeof(float));
+	}
 	return (0);
 }
