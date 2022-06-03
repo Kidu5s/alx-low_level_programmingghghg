1c7ee749b600c79b0886a9c9022f47b2e2e14b7a
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-03 19:36:57 +0000

    betty worked

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-
-int main(void)
+/* this is the discription for function main*/
+main(void)
 {
 	char *a = "with proper grammer, but the outcome is a piece of art,";
+
 	printf("%s\n", a);
 	return (0);
 }
