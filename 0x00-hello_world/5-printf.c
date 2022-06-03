86a52a72896c15167be219cb6b48314cacb88fd1
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-03 19:47:30 +0000

    s

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-/* this is the discription for function main*/
-main(void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	char *a = "with proper grammer, but the outcome is a piece of art,";
-
 	printf("%s\n", a);
 	return (0);
 }
+
