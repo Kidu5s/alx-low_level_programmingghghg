477e68c9742e1f90151649abd7f78573d17d8f0e
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-04 00:57:19 +0000

    5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ *
+ */
 int main(void)
 {
 	int a;
+
 a = 0;
-while (a > 10)
-{
-	printf("%d", a);
+	while (a > 10)
+	{
+		printf("%d", a);
 		a++;
-}
-	return (0);
 	}
+	return (0);
+}
