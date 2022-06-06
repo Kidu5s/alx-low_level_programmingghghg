001365b9eda5d7d2a71c992523d737ce935e4ed6
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-06 17:35:49 +0000

    num with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num_with_comma;
+
+num_with_comma = '0';
+	while (num_with_comma <= '9')
+{
+		putchar(num_with_comma);
+		putchar(',');
+		putchar(' ');
+}
+	return (0);
+}
