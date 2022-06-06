66d2746fdc5c57bca3757af38887c862cd7774a8
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-06 18:38:14 +0000

    num comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = '0'; num1 <= '9'; num1++)
+	{
+		for (num2 = '0'; num2 <= '9'; num2++)
+		{
+			if (num1 < num2)
+				putchar(num1);
+				putchar(num2);
+				putchar(',');
+				putchar(' ');
+		}
+	}
+				putchar('\n');
+	return (0);
+}
