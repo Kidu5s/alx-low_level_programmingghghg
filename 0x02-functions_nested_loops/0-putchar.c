97bc2473f2be0d05430cf9bc13b3b4397e2a8b3a
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-07 10:41:57 -0700

    dont get putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char * printPutchar = "_putchar";
+
+	putchar(*printPutchar);
+	putchar('\n');
+	return (0);
+}
