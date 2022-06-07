6fccbda74591d74fd7fe84b863472f9044909868
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-07 11:54:23 -0700

    compromised

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
 
 /**
+ * _putchar - writes the character x to stdout
+ * @x: the character to print
+ *
+ * Return: always 0
+ */
+int _putchar(void)
+{
+	return (0);
+}
+
+/**
  * main - Entry point
  *
  * Return: always 0
  */
 int main(void)
 {
-	char * printPutchar = "_putchar";
+	char a, b, c, d, e, f, g, h;
 
-	putchar(*printPutchar);
+	a = '_';
+	b = 'p';
+	c = 'u';
+	d = 't';
+	e = 'c';
+	f = 'h';
+	g = 'a';
+	h = 'r';
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar(d);
+	putchar(e);
+	putchar(f);
+	putchar(g);
+	putchar(h);
 	putchar('\n');
 	return (0);
 }
