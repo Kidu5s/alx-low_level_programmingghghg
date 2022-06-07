e241e86aa2544da0ecd4f1beef26d6e1e0741b53
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-07 13:44:58 -0700

    no 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphaLower;
+
+	alphaLower = 'a';
+	while (alphaLower <= 'z')
+	{
+		_putchar(alphaLower);
+		alphaLower++;
+	}
+	_putchar('\n);
+}
