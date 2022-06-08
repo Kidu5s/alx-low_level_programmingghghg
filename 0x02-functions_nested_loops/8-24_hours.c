fe8981747b515e7fc31a072bad0fc4e008819b2e
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-07 20:01:49 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,21 +11,21 @@ void jack_bauer(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-	for (j = '0'; j <= '9'; j++)
-	{
-	for (k = '0'; k <= '9'; k++)
-	{
-	for (l = '0'; l <= '9'; l++)
-	{
-		if ((i <= '2' && j <= '3') && (k <= '5' && l <= '9'))
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				for (l = '0'; l <= '9'; l++)
 				{
-					_putchar(i);
-					_putchar(j);
-					_putchar(':');
-					_putchar(k);
-					_putchar(l);
-					_putchar('\n');
-				}
+					if ((i <= '2' && j <= '3') && (k <= '5' && l <= '9'))
+					{
+						_putchar(i);
+						_putchar(j);
+						_putchar(':');
+						_putchar(k);
+						_putchar(l);
+						_putchar('\n');
+					}
 				}
 			}
 		}
