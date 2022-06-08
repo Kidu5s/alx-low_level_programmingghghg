5226ecd66be4c9cbf0ab462f0b5b4724be9c9e6d
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-07 19:45:50 -0700

    8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,8 +2,8 @@
 
 /**
  * print_last_digit - prints the last digit
- * @i: Where the number is 
- * 
+ * @i: Where the number is
+ *
  * Return: always 0
  */
 int print_last_digit(int i)
@@ -11,6 +11,5 @@ int print_last_digit(int i)
 	int print_last_digit;
 
 	print_last_digit = i % 10;
-	_putchar(print_last_digit);
-	return (0);
+	return (print_last_digit);
 }
