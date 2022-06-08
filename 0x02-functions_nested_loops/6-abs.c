a12809f136296b58243f2b51bb92eda81135bfe2
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-07 18:51:10 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value
+ * @: where you put the numbers
+ *
+ * Return: always 0
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		return (i = i * -1);
+	}
+		return (i);
+}
