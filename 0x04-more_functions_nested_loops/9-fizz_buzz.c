dbe664f7a98a1f7b56f90617678a6d75093fd6eb
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-10 13:53:29 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - replace the number with words
+ *
+ * Return: always 0
+ */
+
 int main(void)
 {
 	int i;
-	
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
@@ -18,11 +24,11 @@ int main(void)
 		{
 			printf("Fizz ");
 		}
-		else 
+		else
 		{
 			printf("%d ", i);
 		}
 	}
 	printf("\n");
-	return 0;
+	return (0);
 }
