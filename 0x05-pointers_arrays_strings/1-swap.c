c13721b59a1ed4a5406b4260e85b99c92af98190
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-13 04:01:30 -0700

    swaping two integer with pointer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two integer
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
