60e7d6d0d811a3785c4a976ceb993d7981535fdb
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-14 09:40:11 -0700

    unix is harsd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 * main - Entry point
 *
@@ -7,65 +6,20 @@
 */
 int main(void)
 {
-	putchar('a');
-	putchar('n');
-	putchar('d');
-	putchar(' ');
-	putchar('t');
-	putchar('h');
-	putchar('a');
-	putchar('t');
-	putchar(' ');
-	putchar('p');
-	putchar('i');
-	putchar('e');
-	putchar('c');
-	putchar('e');
-	putchar(' ');
-	putchar('o');
-	putchar('f');
-	putchar(' ');
-	putchar('a');
-	putchar('r');
-	putchar('t');
-	putchar(' ');
-	putchar('i');
-	putchar('s');
-	putchar(' ');
-	putchar('u');
-	putchar('s');
-	putchar('e');
-	putchar('f');
-	putchar('u');
-	putchar('l');
-	putchar('"');
-	putchar(' ');
-	putchar('-');
-	putchar(' ');
-	putchar('D');
-	putchar('o');
-	putchar('r');
-	putchar('a');
-	putchar(' ');
-	putchar('K');
-	putchar('o');
-	putchar('r');
-	putchar('p');
-	putchar('a');
-	putchar('r');
-	putchar(',');
-	putchar(' ');
-	putchar('2');
-	putchar('0');
-	putchar('1');
-	putchar('5');
-	putchar('-');
-	putchar('1');
-	putchar('0');
-	putchar('-');
-	putchar('1');
-	putchar('9');
-	putchar('\n');
+	int i;
+	char a[60] = {'a', 'n', 'd', ' ', 't', 'h', 'a', 't',
+		 ' ', 'p', 'i', 'e', 'c', 'e', ' ', 'o', 'f',
+		 ' ', 'a', 'r', 't', ' ', 'i', 's', ' ', 'u',
+		 's', 'e', 'f', 'u', 'l', '"', ',', ' ', '-',
+		 'D', 'o', 'r', 'a', 'K', 'o', 'r', 'p', 'a',
+		 'r', '2', '0', '1', '5', '-', '1', '0', '-',
+		 '1', '9'};
 
-return (1);
+	for (i = 0; i <= 60; i++)
+	{
+		putchar(a[i]);
+	}
+		putchar('\n');
+
+	return (1);
 }
