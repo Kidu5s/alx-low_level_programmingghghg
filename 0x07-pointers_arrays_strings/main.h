d9c6ac0eb11f91ec97e27f439b9f3520ad795d45
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-20 11:46:53 -0700

    ushingthe standard library

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@ void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 unsigned int_strspn(char *s, char *accept);
-char *_strchar(char *s, char c);
+char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int size);
