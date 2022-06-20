43c85630fa0742ce0d3cc754edeb1c5b3ddb9939
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-20 07:50:23 -0700

    update 8-24_hour

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,6 @@
 #include "main.h"
-
 /**
 * jack_bauer - prints every min and hour of a day
-*
 * Return: to 0
 */
 void jack_bauer(void)
@@ -11,30 +9,32 @@ void jack_bauer(void)
 
 	for (i = 0; i <= 2; i++)
 	{
-	for (j = 0; j <= 9; j++)
-	{
-	for (k = 0; k <= 5; k++)
-	{
-	for (l = 0; l <= 9; l++)
-	{
-	if (i < 2)
-	{
-	_putchar(i + '0');
-	_putchar(j + '0');
-	_putchar(':');
-	_putchar(k + '0');
-	_putchar(l + '0');
-	_putchar('\n');
-	}
-	else if (i == 2 && j < 4)
-	{
-	_putchar(i + '0');
-	_putchar(j + '0');
-	_putchar(':');                                                          
-	_putchar(k + '0');                                                     		_putchar(l + '0');                                                            	_putchar('\n');
-	}
-	}
-	}
-	}
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					if (i < 2)
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(':');
+						_putchar(k + '0');
+						_putchar(l + '0');
+						_putchar('\n');
+					}
+					else if (i == 2 && j < 4)
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(':');
+						_putchar(k + '0');
+						_putchar(l + '0');
+						_putchar('\n');
+					}
+				}
+			}
+		}
 	}
 }
