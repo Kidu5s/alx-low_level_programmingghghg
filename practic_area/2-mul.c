9684772058220d3da6de417c1b7eddee45738bca
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-30 11:56:46 +0000

    practice

---
+++ practic_area/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multipiles two integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: Result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
