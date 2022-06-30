33bdc76a49e947166be05cac99438dd490260bb2
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-30 14:14:29 +0000

    digona fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,7 +3,7 @@
 /**
  * print_diagonal - prints digonal lines
  * @n: the number of digonals
- * 
+ *
  * Return: void
  */
 void print_diagonal(int n)
@@ -11,24 +11,16 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	for (i = 1; i <= n; i++)
-	{
-		for (j = 1; j <= n; j++)
-		{
-			if (j == i)
-			{
-			_putchar('\\');
-			}
-			else if (j <= 0)
-			{
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar(' ');
-			}
-		}
+	if (n <= 0)
 		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n; j++)
+				_putchar(' ');
+				_putchar('\\');
+				_putchar('\n');
+		}
 	}
-	_putchar('\n');
 }
