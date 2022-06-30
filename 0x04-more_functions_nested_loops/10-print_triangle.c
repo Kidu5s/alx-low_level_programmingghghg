819de3f4f52a4025ccf704566354df8a2b4c158a
Kidus Zelalem <zkidu5s@gmail.com>
2022-06-30 16:16:38 +0000

    Mirror triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,17 +10,21 @@ void print_triangle(int size)
 	int i;
 	int j;
 
-	for (i = 0; i < size; i++)
+	if (size <= 0)
+		_putchar('\n');
+	else
 	{
-		for (j = 0; j <= size; j--)
+		for (i = 1; i <= size; i++)
 		{
-			if (j > 0)
+			for (j = i; j <= size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
-	if (size <= 0)
-		_putchar('\n');
 }
