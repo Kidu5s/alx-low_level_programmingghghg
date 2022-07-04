4a89b2dea26f0cf14a0a4ccae25c9821f65b6127
Kidus Zelalem <zkidu5s@gmail.com>
2022-07-04 11:30:34 -0700

    puts_rescursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,8 +10,9 @@ void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_putchar(*s);
+		_putchar(*s); /* if put in front will print normaly */
 		_puts_recursion(s + 1);
+		/* _putchar(*s); if put down here will print revers */
 	}
 	
 	if (*s == '\0')
