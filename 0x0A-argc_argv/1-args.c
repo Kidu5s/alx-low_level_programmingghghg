3eef950a69ce6ea0bd63c211ccf1700d3e570026
Kidus Zelalem <zkidu5s@gmail.com>
2022-07-05 13:15:57 -0700

    my first argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/** 
+ * main - Entry point
+ * @argc: counts the argument passed to it
+ * @argv: prints the argument passed to it
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	
+	return (0);
+}
