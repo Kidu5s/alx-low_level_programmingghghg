2a7cc242da51cfbb52ce5a251b9a3904ae0f8d4e
Kidus Zelalem <zkidu5s@gmail.com>
2022-07-07 15:41:15 -0700

    addition argv[]

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,12 +13,7 @@ int main(int argc, char *argv[])
 	int i;
 	int sum = 0;
 
-	if (argc < 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
+	if (arg > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
