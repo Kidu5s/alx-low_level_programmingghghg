1a8eca36e69767f571152cc413b7dca902ca14c6
Kidus Zelalem <zkidu5s@gmail.com>
2022-07-09 05:59:03 -0700

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,9 +11,13 @@
 int _prime_number_2(int i, int j)
 {
 	if (i == j)
+	{
 		return (1);
+	}
 	else if (i % j == 0)
+	{
 		return (0);
+	}
 		return (_prime_number_2(i, j + 1));
 }
 
@@ -28,5 +32,5 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (prime2(n, 2));
+	return (_prime_number_2(n, 2));
 }
