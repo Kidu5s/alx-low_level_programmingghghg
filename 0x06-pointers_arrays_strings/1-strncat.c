0be2e0c7c2d0bbe945bdef463be4297d22eac007
Kidus Zelalem <zkidu5s@gmail.com>
2022-07-09 03:44:41 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strncat - concatenates the string
@@ -12,6 +11,24 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0' && n > j)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	if (n > 0)
+		dest[i] = '\0';
 	return (dest);
 }
