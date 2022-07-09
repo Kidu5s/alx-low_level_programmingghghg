7694fa662a7039768bca87054ed8592b08600317
Kidus Zelalem <zkidu5s@gmail.com>
2022-07-09 03:44:12 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,13 +14,18 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
 	{
-		;
+		i++;
 	}
-	for (j = 0;src[j] != '\0'; j++)
+
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
+		j++;
+		i++;
 	}
 	dest[i] = '\0';
 	return (dest);
