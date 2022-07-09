a8c2cb7b8186eb81f8b55c1f866ddd3176cd087b
Kidus Zelalem <zkidu5s@gmail.com>
2022-07-08 18:31:05 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,11 +4,27 @@
 
 /**
  * rev_string - revers the string
- * @s: the string
- *
- * Return: revers string
+ * @s: pointer to char
+ * 
+ * Return: nothing
  */
 void rev_string(char *s)
 {
-	strsep(s);
+	int length;
+	int i;
+	int j;
+	int tmp;
+
+	length = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length += 1;
+	}
+
+	for (j = 0; j < (length / 2); j++)
+	{
+		tmp = s[j];
+		s[j] = s[length - j - 1];
+		s[length - j - 1] = tmp;
+	}
 }
