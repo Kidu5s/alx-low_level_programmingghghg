6f09b8918f2c6243c2cc7c33a4f36f6dccccd0a9
Kidus Zelalem <zkidu5s@gmail.com>
2022-07-23 01:19:52 -0700

    strncat correction

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,21 +14,14 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
-	i = 0;
-	j = 0;
-
-	while (dest[i] != '\0')
+	for (i = 0; dest[i] != '\0'; i++)
 	{
-		i++;
 	}
-
-	while (src[j] != '\0' && n > j)
+	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
 		dest[i] = src[j];
-		j++;
 		i++;
 	}
-	if (n > 0)
-		dest[i] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
