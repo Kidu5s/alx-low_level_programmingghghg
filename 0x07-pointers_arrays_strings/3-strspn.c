061d5e3f4e34baa82fd995bbdf77ec31895815f1
Kidus Zelalem <zkidu5s@gmail.com>
2022-07-26 12:17:35 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strspn - gets the length of prefix
@@ -9,5 +8,26 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	return (strspn(s, accept));
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int len = 0;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				len++;
+			}				
+			if (s[j + 1] < 'A' || s[j + 1] > 'Z')
+			{
+				if (s[j + 1] < 'a' || s[j + 1] > 'z')
+				{
+					break;
+				}
+			}
+		}
+	}
+	return (len);
 }
