58a56b971226c53b432459b31b12185fade455b3
Kidus Zelalem <zkidu5s@gmail.com>
2022-07-26 10:11:42 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-char *_memcpy(char *dest, char *str, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-		dest[i] = str[i];
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[i] = src[i];
 	return (dest);
 }
