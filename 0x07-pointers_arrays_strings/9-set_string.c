e8b7700d1c0cdbdda956febf709c779cf651f622
Kidus Zelalem <zkidu5s@gmail.com>
2022-07-27 03:47:27 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_string - set the value of a pointer to char
+ * @s: a pointer that holds the address of a pointer to char
+ * @to: a pointer to char
+ *
+ * Return: Nothing
+ */
+void set_string(char **s, char *to)
+{
+	/* replace the value of s (address of a pointer) 
+	 * with the value of to (address of a char)
+	 */
+	*s = to;
+}
