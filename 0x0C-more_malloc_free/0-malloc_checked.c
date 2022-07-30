d2223bdd5530fb8ebb001d401333288046129133
Kidus Zelalem <zkidu5s@gmail.com>
2022-07-30 09:42:41 -0700

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - lksdflk
+ * @b: sadklflk
+ *
+ * Return: klasdlkjf
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b * sizeof(int));
+
+	return (ptr);
+	exit(98);
+}
