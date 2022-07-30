f2905c2a9e9225d7b67e7d7b1e8f7531bc7345d0
Kidus Zelalem <zkidu5s@gmail.com>
2022-07-30 04:14:24 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,14 +16,14 @@ char *_strdup(char *str)
 	int i;
 	int j;
 
+	if (str == NULL)
+		return (NULL);
 	for (j = 0; str[j] != '\0'; j++)
 		;/* Will increament j */
 	j++; /* in order to add the '\0' */
 
 	ptr = malloc(sizeof(char) * j);
 
-	if (str == NULL)
-		return (NULL);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
