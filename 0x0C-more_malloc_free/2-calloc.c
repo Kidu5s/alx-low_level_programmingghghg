b650b134c6045834a1ace6a9ff8ff336e52db123
Kidus Zelalem <zkidu5s@gmail.com>
2022-07-31 15:06:51 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,15 +12,15 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	void *ptr;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = malloc(nmemb * size);
+	ptr = (void *)malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
-		ptr[i] = 0;
+		*(ptr + i) = 0;
 
 	return (ptr);
 }
