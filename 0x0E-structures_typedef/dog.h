c9a466ca71ea5bf3e39d6c4ef3ad7278ca9aeffe
Kidus Zelalem <zkidu5s@gmail.com>
2022-08-10 05:18:40 -0700

    init

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,22 +6,18 @@
  * @name: the name of the dog
  * @age: the age of the dog
  * @owner: the owner of the dog
+ * dog_t - typedef
  *
  * Description: this structure is about a dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/**
- * struct dog - a strct of dog
- * dog_t - typedef
- */
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owenr);
 void free_dog(dog_t *d);
 
