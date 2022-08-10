81d0ea478216b576d89d3629925abf4ba44a4d53
Kidus Zelalem <zkidu5s@gmail.com>
2022-08-10 03:53:55 -0700

    sturct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initalize the struct dog
+ * @d: the struct that holds the memory address
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @ownere: the owner of the dog
+ *
+ * Return: Nothing
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * sturct dog - a structer of dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: this structure is about a dog
+ */
 struct dog
 {
 	char *name;
@@ -9,6 +17,9 @@ struct dog
 };
 void inti_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * dog_t - typedef
+ */
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owenr);
 void free_dog(dog_t *d);
