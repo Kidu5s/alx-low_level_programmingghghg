e8960d56108760ea36a8d2d2d2e4fcb76946f689
Kidus Zelalem <zkidu5s@gmail.com>
2022-10-09 03:19:08 -0700

    times tabel

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,60 +1,51 @@
 #include "main.h"
+
 /**
-*
-*  * print_times_table - print the n times table
-*
-*   * @n: the times
-*
-*    *
-*
-*     * Return: void
-*
-*      */
+ * print_times_table - print the n times table
+ * @n: the times
+ * 
+ * Return: void
+ */
 
 void print_times_table(int n)
-
 {
+	int i, j, k;
 
-int i;
-
-int j;
-
-int k;
-
-
-
-if (n < 15 && n > 0)
-
-{
-
-for (i = 0; i <= n; i++)
-
-{
-
-for (j = 0; j <= n; j++)
-
-{
-
-k = i * j;
-
-if (j != 0)
-
-{
-
-_putchar(',');
-
-_putchar(' ');
-
+	if (n < 15 && n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = i * j;
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (k <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k > 9 && k < 100)
+				{
+					_putchar(' ');
+					_putchar(k / 10 + '0');
+					_putchar(k % 10 + '0');
+				}
+				else
+				{
+					_putchar(k / 100 + '0');
+					_putchar((k / 10) % 10 + '0');
+					_putchar(k % 10 + '0');
+				}
+			}
+				_putchar('\n');
+		}
+	}
+	else
+	{
+	}
 }
-
-if (k <= 9)
-
-{
-
-_putchar(' ');
-
-_putchar(' ');
-
-_putchar(k + '0');
-
-
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Rturn: Always 0.
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
