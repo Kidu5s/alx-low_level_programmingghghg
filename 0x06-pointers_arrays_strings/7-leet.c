ead62c2ec246143d0cd9b8f311182df3f1a318b9
Kidus Zelalem <zkidu5s@gmail.com>
2022-12-07 10:12:39 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,21 +6,20 @@
 * Return: the encoded string.
 */
 
-char *leet(char *s)
+char *leet(char *str)
 {
 	int i = 0, j = 0;
-	char array_leet[] = {'4', '3', '1', '0', '7'};
-	char array_up[] = {'A', 'E', 'L', 'O', 'T'};
-	char array_low[] = {'a', 'e', 'l', 'o', 't'};
+	char num[] = {'4', '3', '1', '0', '7'};
+	char CC[] = {'A', 'E', 'L', 'O', 'T'};
+	char cc[] = {'a', 'e', 'l', 'o', 't'};
 
-	while (s[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 5; j++)
 		{
-			if (s[i] == array_low[j] || s[i] == array_up[j])
-				s[i] = array_leet[j];
+			if (str[i] == CC[j] || s[i] == cc[j])
+				s[i] = num[j];
 		}
-		i++;
 	}
-	return (s);
+	return (str);
 }
