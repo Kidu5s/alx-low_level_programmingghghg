b64d668869c38f9093d5c5eac9af13d761eb415d
Kidus Zelalem <zkidu5s@gmail.com>
2022-12-09 05:29:21 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,19 +10,24 @@ char *rot13(char *str)
 {
 	int i;
 	int j;
-	char aA[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
-	char nN[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+	char aA_zZ[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	char nN_zZ[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; j < 27; j++)
+		for (j = 0; j < 53; j++)
 		{
-			if (str[i] == aA[j])
-				str[i] = nN[j];
-			else if (str[i] == nN[j])
-				str[i] = aA[j];
+			if (str[i] == aA_zZ[j])
+			{
+				str[i] = nN_zZ[j];
+				break;
+			}
 		}
 	}
 	return (str);
