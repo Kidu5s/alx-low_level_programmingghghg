05c92ac4a40933d7191a49f71a6f67ecb51e95c8
Kidus Zelalem <zkidu5s@gmail.com>
2022-12-11 04:20:05 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,8 +9,15 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
+	{
+		/* prints the first char of the str b/c it
+		 * pointing to the mem address of the first char
+		 */
+		_putchar(*s);
+		/* will increment the memory address */
+		_puts_recursion(s + 1);
+	}
+	else
 		_putchar('\n');
-	_putchar(*s);
-	_puts_recursion(s + 1);
 }
