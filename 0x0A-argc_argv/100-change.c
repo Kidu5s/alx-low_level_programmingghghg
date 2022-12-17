19389e40063c6864a0a3e69868328e8f34f5c38d
Kidus Zelalem <zkidu5s@gmail.com>
2022-12-17 10:19:31 -0800

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,57 +1,66 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 /**
-* minCoins - multiplies two numbers
-* @argc: the number of passed arguments
-* @argv: an array of passsed arguments
-*
-* Return: 0 on success, 1 on error
-*/
-int minCoins(int coins[], int m, long cents)
+ * main - prints the minimum number of coins to make 
+ * change for an amount of money
+ * @argc: count the nubmer of arguments passed in
+ * @argv: holds all teh argument passed in
+ *
+ * Return: 0 on Success
+ * 1 on Error.
+ */
+int main(int argc, char *argv[])
 {
-	int i;
-	int res = INT_MAX;
-	if (cents == 0)
-		return (0);
+	int i, j, k, l, m;
 
-	for (i = 0; i < m; i++)
+	if (argc == 2)
 	{
-		if (coins[i] <= cents)
+		for (argc = 1; argv[argc]; argc++)
 		{
-			int sub_res = minCoins(coins, m, cents - coins[i]);
-			
-			if ((sub_res != INT_MAX) && ((sub_res + 1) < res))
-				res = sub_res + 1;
+			i = atoi(argv[argc]);
+			m = i / 25 + i % 25 / 10 + i % 25 % 10 / 5 + i % 25 % 10 % 5 / 2 + i % 25 % 10 % 5 % 2;
+			l = i / 10 + i % 10 / 5 + i % 10 % 5 / 2 + i % 10 % 5 % 2;
+			k = i / 5 + i % 5 / 2 + i % 5 % 2;
+			j = i / 2 + i % 2;
+			if (i < 0)
+				printf("0\n");
+			else if (i >= 1 && i < 2)
+				printf("%d\n", i);
+			else if (i >= 2 && i < 5)
+			{
+				if (i % 2 == 0)
+					printf("%d\n", i / 2);
+				else
+					printf("%d\n", j);
+			}
+			else if (i >= 5 && i < 10)
+			{
+				if (i % 5 == 0)
+					printf("%d\n", i / 5);
+				else
+					printf("%d\n", k);
+			}
+			else if (i >= 10 && i < 25)
+			{
+				if (i % 10 == 0)
+					printf("%d\n", i / 10);
+				else
+					printf("%d\n", l);
+			}
+			else
+			{
+				if (i % 25 == 0)
+					printf("%d\n", i / 25);
+				else
+					printf("%d\n", m);
+			}
 		}
 	}
-	return (res);
-}
-
-/**
-* main - multiplies two numbers
-* @argc: the number of passed arguments
-* @argv: an array of passsed arguments
-*
-* Return: 0 on success, 1 on error
-*/
-int main(int argc, char  **argv)
-{
-	long cents;
-	int coins[] = {25, 10, 5, 2, 1};
-	int m = sizeof(coins)/sizeof(coins[0]);
-	
-	if (argc != 2)
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi(argv[1]);
-	if (cents < 0)
-		printf("0");
-	else
-		printf("%d\n", minCoins(coins, m, cents));
 	return (0);
 }
