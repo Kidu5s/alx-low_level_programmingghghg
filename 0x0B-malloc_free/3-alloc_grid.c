80b11cb2f78b150bd13268c311b9e585abcd984b
Kidu5s <zkidu5s@gmail.com>
2023-02-10 07:52:11 +0300

    check the code

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,14 +14,13 @@ int **alloc_grid(int width, int height)
 
 	if (width == 0 || height == 0)
 		return (NULL);
-	ptr = malloc(width * sizeof(*ptr));
-	for (i = 0; i < width; i++)
-		ptr[i] = malloc(height * sizeof(ptr));
+	ptr = malloc(height * sizeof(*ptr));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < height; j++)
+		ptr[i] = malloc(width * sizeof(ptr));
+		for (j = 0; j < width; j++)
 		{
 			ptr[i][j] = 0;
 		}
