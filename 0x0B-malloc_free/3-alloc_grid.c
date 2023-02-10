311fc60d18f09db2d0992766a593fa42c73d57b4
Kidu5s <zkidu5s@gmail.com>
2023-02-10 08:12:30 +0300

    check the code

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	check = 0;
 	if (width == 0 || height == 0)
 		return (NULL);
-	ptr = malloc(height * sizeof(ptr));
+	ptr = malloc(height * sizeof(int *));
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
