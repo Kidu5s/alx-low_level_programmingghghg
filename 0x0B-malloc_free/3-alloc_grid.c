f3573c314a44ceea390958cc592039df6ce95f67
Kidu5s <zkidu5s@gmail.com>
2023-02-10 07:24:06 +0300

    check the code

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,48 +1,30 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
-* alloc_grid - returns a pointer to a 2 dimensional array of in    tegers
-* @width: columns
-* @height: rows
-*
-* Return: Double Pointer
-*/
-
+ * alloc_grid - osdfj
+ * @width: lsdkjf
+ * @height: lsdkjf
+ *
+ * Return: lskdjfkladfk
+ */
 int **alloc_grid(int width, int height)
 {
-	int **s;
-	int i, j, k, fr;
+	int **ptr;
+	int i, j;
 
-	fr = 0;
-	if (width <= 0 || height <= 0)
-		return (0);
-
-	s = malloc(height * sizeof(int *));
-	if (s == 0)
-		return (0);
-
-	for (i = 0; i < height; i++)
+	if (width == 0 || height == 0)
+		return (NULL);
+	ptr = malloc(width * sizeof(*ptr));
+	for (i = 0; i < width; i++)
+		ptr[i] = malloc(height * sizeof(ptr));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < width; i++)
 	{
-		*(s + i) = malloc(width * sizeof(int));
-		if (*(s + i) == 0)
+		for (j = 0; j < height; j++)
 		{
-			fr = 1;
-			break;
-		}
-		for (j = 0; j < width; j++)
-		{
-			s[i][j] = 0;
+			ptr[i][j] = 0;
 		}
 	}
-	if (fr == 1)
-	{
-		for (k = 0; k <= i; k++)
-		{
-			free(*(s + k));
-		}
-		free(s);
-	}
-	return (s);
+	return (ptr);
 }
