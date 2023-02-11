464c3b124fe7ac26decedea58180ad655b41b17c
Kidu5s <zkidu5s@gmail.com>
2023-02-11 22:46:53 +0300

    array range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,29 +1,59 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
+int array_length(int min, int max);
 /**
- * array_range - creates an array of integers
- * @min: the minimum value
- * @max: the maximum value
+ * array_range - creates an array of integer
+ * @min: the initial value of the array
+ * @max: the finall value of the array
  *
- * Return: the pointer to the newly created array
+ * Return: pointer to the newely created arraky
+ * on faliure NULL
+ * if min > max return NULL
  */
 int *array_range(int min, int max)
 {
-	int i, len;
 	int *ptr;
+	int len, i;
 
 	if (min > max)
 		return (NULL);
 	for (len = 0; min != max; len++)
 		min++;
 	len++;
-	ptr = malloc(sizeof(int) * (len++));
+
+	len = array_length(min, max);
+
+	ptr = malloc(len * sizeof(int));
 	if (ptr == NULL)
 		return (NULL);
+
 	for (i = 0; min <= max; i++)
 	{
+		/* initialize ptr */
 		ptr[i] = min;
+		/* increment min til max */
 		min++;
 	}
+
 	return (ptr);
+}
+/**
+ * array_length - gets the length of the arry
+ * @min: the first element of the array
+ * @max: the last element of the array
+ *
+ * Return: the length
+ */
+int array_length(int min, int max)
+{
+	int len;
+
+	/* the length of the array */
+	for (len = 0; min != max; len++)
+		min++;
+	len++;
+
+	return (len);
 }
