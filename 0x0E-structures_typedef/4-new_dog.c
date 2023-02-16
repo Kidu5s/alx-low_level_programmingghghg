d91cfb485cb5877239c15a740b3768c93cb01d1f
Kidu5s <zkidu5s@gmail.com>
2023-02-16 10:59:11 +0300

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 
+char *_strcpy(char *str);
 /**
  * new_dog: a struct of dog that creats new dog
  * @name: the name of the dog
@@ -12,12 +13,45 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	char *cpy_name;
+	char *cpy_owner;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = name;
+	cpy_name = _strcpy(name);
+	if (cpy_name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	cpy_owner = _strcpy(owner);
+	if (cpy_owner == NULL)
+	{
+		free(new_dog);
+		free(cpy_name);
+		return (NULL);
+	}
+	
+	new_dog->name = cpy_name;
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = cpy_owner;
 	return (new_dog);
 }
+
+char *_strcpy(char *str)
+{
+	int len;
+	int i;
+	char *ptr;
+
+	for (len = 0; str[len]; len++)
+		;
+	len++;
+	ptr = malloc(sizeof(char) * len);
+	for (i = 0; str[i]; i++)
+		ptr[i] = str[i];
+	ptr[i] = '\0';
+	return (ptr);
+}
+
