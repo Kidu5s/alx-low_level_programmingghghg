e8615302ab489b97134a6b5330c9ee61b4c6e324
Kidu5s <zkidu5s@gmail.com>
2023-03-06 12:47:53 +0300

    excute before the main func

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,11 +2,14 @@
 
 void first(void) __attribute__((constructor));
 /**
- * func - prints a string
- * 
+ * first - prints a string before the main function is exicuted
+ *
  * Return: Nothing
  */
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	char *s;
+
+	s = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+	printf("%s", s);
 }
