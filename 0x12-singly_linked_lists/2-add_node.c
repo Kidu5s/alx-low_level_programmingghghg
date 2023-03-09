32c76e0267711dcd12e0055f21c2e2672d051503
Kidu5s <zkidu5s@gmail.com>
2023-03-09 18:17:40 +0300

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -25,12 +25,13 @@ list_t *add_node(list_t **head, const char *str)
 	temp->len = len;
 	temp->next = NULL;
 	if (*head == NULL)
-		temp->next = NULL;
-	else
-		temp->next = *head;
+	{
+		*head = temp;
+       		return (*head);
+	}
+	temp->next = *head;
 	*head = temp;
 	return (*head);
-
 }
 
 /**
