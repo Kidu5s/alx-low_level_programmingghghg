f786564a11a944a1994612302d88069db01d8e75
Kidu5s <zkidu5s@gmail.com>
2023-03-09 18:37:09 +0300

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,43 +1,53 @@
 #include "lists.h"
-int len(const char *str);
+
 /**
- * add_node - Creates a node and adds it to a linked list
- * @head: Pointer to head of the linked list
- * @str: String to assigned to the node
- * Return: Pointer to the head of the linked list
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: the first of the list
+ * @str: the string to be added
+ *
+ * Return: the address of the new element or NULL if it failed
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *temp;
+	char *ptr;
+	size_t len;
 
 	if (head == NULL)
 		return (NULL);
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	ptr = strdup(str);
+	len = _strlen(ptr);
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
 		return (NULL);
-
-	if (*head == NULL) /*Initialzes the next pointer to NULL*/
-		new_node->next = NULL;
-	else /*Moves the new node to the head*/
-		new_node->next = *head;
-	new_node->str = strdup(str);
-	new_node->len = len(str);
-	*head = new_node;
+	temp->str = ptr;
+	temp->len = len;
+	temp->next = NULL;
+	if (*head == NULL)
+	{
+		temp->next = NULL;
+	}
+	else
+	{
+		temp->next = *head;
+	}
+	temp->next = *head;
+	*head = temp;
 	return (*head);
 }
 
 /**
- * len - Calculates the length of a string.
- * @str: Constant string
- * Return: The length of the string
+ * _strlen - finds the length of a string
+ * @ptr: the string
+ *
+ * Return: the length of the string
  */
-int len(const char *str)
+size_t _strlen(char *ptr)
 {
 	int i;
 
-	if (str == NULL)
-		return (0);
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; ptr[i]; i++)
 		;
 	return (i);
 }
