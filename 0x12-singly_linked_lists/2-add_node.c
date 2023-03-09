a64ca9a4d4a4afefb291c9b7fa7ce274e5d40864
Kidu5s <zkidu5s@gmail.com>
2023-03-09 18:42:00 +0300

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -21,6 +21,9 @@ list_t *add_node(list_t **head, const char *str)
 	temp = malloc(sizeof(list_t));
 	if (temp == NULL)
 		return (NULL);
+	temp->str = ptr;
+	temp->len = len;
+
 	if (*head == NULL)
 	{
 		temp->next = NULL;
@@ -29,9 +32,7 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		temp->next = *head;
 	}
-	temp->str = ptr;
-	temp->len = len;
-	temp->next = *head;
+	
 	*head = temp;
 	return (*head);
 }
