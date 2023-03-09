5a3603785ce80b179a31070b28dd63861e9e8a2a
Kidu5s <zkidu5s@gmail.com>
2023-03-09 18:54:38 +0300

    2-add_node.c

---
+++ 0x12-singly_linked_lists/main.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+size_t _strlen(char *str);
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: the first of the list
+ * @str: the string to be added
+ *
+ * Return: the address of the new element or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+	char *ptr;
+	size_t len;
+
+	if (head == NULL)
+		return (NULL);
+
+	ptr = strdup(str);
+	len = _strlen(ptr);
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		temp->next = NULL;
+	}
+	else
+	{
+		temp->next = *head;
+	}
+	temp->str = ptr;
+	temp->len = len;
+	*head = temp;
+	return (*head);
+}
+
+/**
+ * _strlen - finds the length of a string
+ * @ptr: the string
+ *
+ * Return: the length of the string
+ */
+size_t _strlen(char *ptr)
+{
+	int i;
+
+	for (i = 0; ptr[i]; i++)
+		;
+	return (i);
+}
