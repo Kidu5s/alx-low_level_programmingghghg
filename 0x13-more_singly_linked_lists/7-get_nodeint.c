df9569891915b7c7a20a36dbc0d4f461dd1980fd
Kidu5s <zkidu5s@gmail.com>
2023-03-10 22:49:22 +0300

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,10 +13,15 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp = head;
 	unsigned int i = 0;
 
+	if (head == NULL)
+		return (NULL);
 	for (i = 0; i <	index; i++)
 	{
-		temp = temp->next;
+		if (temp != NULL)
+			temp = temp->next;
+		else
+			return (NULL);
 	}
-	return (temp);
+	return 	(temp);
 }
 
