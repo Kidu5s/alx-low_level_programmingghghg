8c0493ad529515b8720ac680414a3267fd9e405a
Kidu5s <zkidu5s@gmail.com>
2023-03-10 23:00:05 +0300

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_listint - adds all the data in the list
+ * @head: a pointer that holds the address of first element in the list
+ * 
+ * Return: the sum of all the data
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *temp;
+	int sum = 0;
+
+	temp = head;
+	if (head == NULL)
+		return (0);
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
